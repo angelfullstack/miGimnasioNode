d96d0719a5669f1428946ad8964f870ea9fd21b9
angelfullstack <angelfullstack@gmail.com>
2020-03-15 15:24:23 +0100

    creados los métodos básicos y si erutación para clientes getAll(),getById(), create(), deleteById(), updateById(). Falta validator DNI

--- models/cliente.js
+++ models/cliente.js
@@ -1,12 +1,139 @@
 const getAll = () => {
   return new Promise((resolve, reject) => {
-    db.query("select * from clientes", (err, rows) => {
+    db.query("SELECT * FROM clientes", (err, rows) => {
       if (err) reject(err);
       resolve(rows);
     });
   });
 };
 
-module.exports = {
-  getAll: getAll
+const getById = id => {
+  return new Promise((resolve, reject) => {
+    db.query("SELECT * FROM clientes where id = ?", [id], (err, rows) => {
+      if (err) reject(err);
+      resolve(rows);
+    });
+  });
 };
+
+/**
+ *create
+ *@param {string}nombre
+ *@param {string}apellidos
+ *@param {string}direccion
+ *@param {string}email
+ *@param {number}edad
+ *@param {string}sexo
+ *@param {number}cuota
+ *@param {string}fecha_nacimiento -(yyyy:MM:dd)
+ *@param {string}dni
+ *@param {number}fk_profesor
+ */
+const create = ({
+  nombre,
+  apellidos,
+  direccion,
+  email,
+  edad,
+  sexo,
+  cuota,
+  fecha_nacimiento,
+  dni,
+  fk_profesor
+}) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      "INSERT INTO clientes (nombre,apellidos,direccion,email,edad,sexo,fecha_inscripcion,cuota,fecha_nacimiento,dni,fk_profesor) VALUES (?,?,?,?,?,?,?,?,?,?,?)",
+      [
+        nombre,
+        apellidos,
+        direccion,
+        email,
+        edad,
+        sexo,
+        new Date(),
+        cuota,
+        fecha_nacimiento,
+        dni,
+        fk_profesor
+      ],
+      (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      }
+    );
+  });
+};
+
+const deleteById = id => {
+  return new Promise((resolve, reject) => {
+    db.query("DELETE FROM clientes WHERE id = ?", [id], (err, result) => {
+      if (err) reject(err);
+      resolve(result);
+    });
+  });
+};
+
+/**
+ *create
+ *@param {string}nombre
+ *@param {string}apellidos
+ *@param {string}direccion
+ *@param {string}email
+ *@param {number}edad
+ *@param {string}sexo
+ *@param {number}cuota
+ *@param {string}fecha_nacimiento -(yyyy:MM:dd)
+ *@param {string}dni
+ *@param {number}fk_profesor
+ */
+
+const updateById = (
+  {
+    nombre,
+    apellidos,
+    direccion,
+    email,
+    edad,
+    sexo,
+    cuota,
+    fecha_nacimiento,
+    dni,
+    fk_profesor
+  },
+  id
+) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      "UPDATE clientes SET nombre=?,apellidos=?,direccion=?,email=?,edad=?,sexo=?,cuota=?,fecha_nacimiento=?,dni=?,fk_profesor=? WHERE id=?",
+      [
+        nombre,
+        apellidos,
+        direccion,
+        email,
+        edad,
+        sexo,
+        cuota,
+        fecha_nacimiento,
+        dni,
+        fk_profesor,
+        id
+      ],
+      (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      }
+    );
+  });
+};
+
+module.exports = {
+  getAll: getAll,
+  getById: getById,
+  create: create,
+  deleteById,
+  deleteById,
+  updateById: updateById
+};
+
+
--- routes/api/clientes.js
+++ routes/api/clientes.js
@@ -1,10 +1,102 @@
 var router = require("express").Router();
 const Cliente = require("../../models/cliente");
 
-/* GET users listing. */
+/* GET http://localhost:3000/api/clientes/ */
 router.get("/", async (req, res) => {
-  const rows = await Cliente.getAll();
-  res.json(rows);
+  try {
+    const rows = await Cliente.getAll();
+    res.json(rows);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+/* GET http://localhost:3000/api/clientes/ */
+router.get("/:id", async (req, res) => {
+  try {
+    const rows = await Cliente.getById(req.params.id);
+    if (rows.length === 1) {
+      res.json(rows[0]);
+    } else {
+      res.json("Cliente no encontrado");
+    }
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+/* POST http://localhost:3000/api/clientes/ */
+router.post("/", async (req, res) => {
+  try {
+    const result = await Cliente.create(req.body);
+    if (result["affectedRows"] === 1) {
+      res.json(result);
+    } else {
+      res.json({ error: "Error en la petición crear cliente" });
+    }
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+/* DELETE http://localhost:3000/api/clientes/ */
+router.delete("/:id", async (req, res) => {
+  try {
+    const result = await Cliente.deleteById(req.params.id);
+    if (result["affectedRows"] === 1) {
+      res.json(result);
+    } else {
+      res.json({ error: "El cliente no existe" });
+    }
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+/* PUT http://localhost:3000/api/clientes/ */
+router.put("/:id", async (req, res) => {
+  try {
+    let cliente = await Cliente.getById(req.params.id);
+    if (req.body.nombre == undefined) {
+      req.body.nombre = cliente[0].nombre;
+    }
+    if (req.body.apellidos == undefined) {
+      req.body.apellidos = cliente[0].apellidos;
+    }
+    if (req.body.direccion == undefined) {
+      req.body.direccion = cliente[0].direccion;
+    }
+    if (req.body.email == undefined) {
+      req.body.email = cliente[0].email;
+    }
+    if (req.body.edad == undefined) {
+      req.body.edad = cliente[0].edad;
+    }
+    if (req.body.sexo == undefined) {
+      req.body.sexo = cliente[0].sexo;
+    }
+    if (req.body.cuota == undefined) {
+      req.body.cuota = cliente[0].cuota;
+    }
+    if (req.body.fecha_nacimiento == undefined) {
+      req.body.fecha_nacimiento = cliente[0].fecha_nacimiento;
+    }
+    if (req.body.dni == undefined) {
+      req.body.dni = cliente[0].dni;
+    }
+    if (req.body.fk_profesor == undefined) {
+      req.body.fk_profesor = cliente[0].fk_profesor;
+    }
+
+    const result = await Cliente.updateById(req.body, req.params.id);
+    if (result["affectedRows"] === 1) {
+      res.json(result);
+    } else {
+      res.json({ error: "El cliente no existe" });
+    }
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 module.exports = router;

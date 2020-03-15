a1ad9ff330e2d29a8f57299597ca04b67328971d
angelfullstack <angelfullstack@gmail.com>
2020-03-15 13:54:53 +0100

    creados los métodos básicos y si erutación para ejercicios getAll(),getById(), create(), deleteById(), updateById()

--- models/ejercicio.js
+++ models/ejercicio.js
@@ -1,12 +1,75 @@
 const getAll = () => {
   return new Promise((resolve, reject) => {
-    db.query("select * from ejercicios", (err, rows) => {
+    db.query("SELECT * FROM ejercicios", (err, rows) => {
       if (err) reject(err);
       resolve(rows);
     });
   });
 };
 
-module.exports = {
-  getAll: getAll
+const getById = id => {
+  return new Promise((resolve, reject) => {
+    db.query("SELECT * FROM ejercicios where id = ?", [id], (err, rows) => {
+      if (err) reject(err);
+      resolve(rows);
+    });
+  });
 };
+
+/** 
+*create:
+*@param {string}titulo
+*@param {string(hh:mm:ss)}duracion
+*@param {number}repeticiones 
+*/
+const create = ({ titulo, duracion,repeticiones }) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      "INSERT INTO ejercicios (titulo,duracion,repeticiones) VALUES (?,?,?)",
+      [titulo, duracion, repeticiones],
+      (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      }
+    );
+  });
+};
+
+const deleteById = id => {
+  return new Promise((resolve, reject) => {
+    db.query("DELETE FROM ejercicios WHERE id = ?", [id], (err, result) => {
+      if (err) reject(err);
+      resolve(result);
+    });
+  });
+};
+
+/** 
+*update:
+*@param {string} titulo
+*@param {string} duracion -(hh:mm:ss)
+*@param {number}repeticiones - entero <=127
+*/
+const updateById = ({ titulo, duracion,repeticiones }, id) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      "UPDATE ejercicios SET titulo=?,duracion=?,repeticiones=? WHERE id=?",
+      [titulo, duracion,repeticiones, id],
+      (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      }
+    );
+  });
+};
+
+module.exports = {
+  getAll: getAll,
+  getById: getById,
+  create: create,
+  deleteById,
+  deleteById,
+  updateById: updateById
+};
+
+
--- routes/api/ejercicios.js
+++ routes/api/ejercicios.js
@@ -1,10 +1,81 @@
 var router = require("express").Router();
 const Ejercicio = require("../../models/ejercicio");
 
-/* GET users listing. */
+/* GET http://localhost:3000/api/ejercicios/ */
 router.get("/", async (req, res) => {
-  const rows = await Ejercicio.getAll();
-  res.json(rows);
+  try {
+    const rows = await Ejercicio.getAll();
+    res.json(rows);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+/* GET http://localhost:3000/api/ejercicios/ */
+router.get("/:id", async (req, res) => {
+  try {
+    const rows = await Ejercicio.getById(req.params.id);
+    if (rows.length === 1) {
+      res.json(rows[0]);
+    } else {
+      res.json("Ejercicio no encontrado");
+    }
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+/* POST http://localhost:3000/api/ejercicios/ */
+router.post("/", async (req, res) => {
+  try {
+    const result = await Ejercicio.create(req.body);
+    if (result["affectedRows"] === 1) {
+      res.json(result);
+    } else {
+      res.json({ error: "Error en la petición crear ejercicio" });
+    }
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+/* DELETE http://localhost:3000/api/ejercicios/ */
+router.delete("/:id", async (req, res) => {
+  try {
+    const result = await Ejercicio.deleteById(req.params.id);
+    if (result["affectedRows"] === 1) {
+      res.json(result);
+    } else {
+      res.json({ error: "El ejercicio no existe" });
+    }
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+/* PUT http://localhost:3000/api/profesores/ */
+router.put("/:id", async (req, res) => {
+  try {
+    //Aunque se supone que en el ejercicio hay que pasar todos los datos, creo un parseador para que en el caso de que los datos no estén completos en el body se sustituyan por los de la fila original.
+    let ejercicio = await Ejercicio.getById(req.params.id);
+    if (req.body.titulo == undefined) {
+      req.body.titulo = ejercicio[0].titulo;
+    }
+    if (req.body.duracion == undefined) {
+      req.body.duracion = ejercicio[0].experiencia;
+    }
+    if (req.body.repeticiones == undefined) {
+      req.body.repeticiones = ejercicio[0].repeticiones;
+    }
+    const result = await Ejercicio.updateById(req.body, req.params.id);
+    if (result["affectedRows"] === 1) {
+      res.json(result);
+    } else {
+      res.json({ error: "El ejercicio no existe" });
+    }
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 module.exports = router;
